0ae8c3cfed9d5574c3288eeb761b5c1575890819
Liz Martins <elizabeth.martins5@gmail.com>
2023-03-06 14:53:11 -0500

    user.js services

---
+++ client/src/services/User.js
@@ -0,0 +1,31 @@
+import Client from './api'
+
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post('/users/login', data)
+    localStorage.setItem('token', res.data.token)
+    // Set the current signed in users token to localStorage
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/users/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    // Checks if the current token if it exists is valid
+    const res = await Client.get('/users/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
