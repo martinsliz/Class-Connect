891c2efd809e3e82825285e6856f95e81c6a444f
Liz Martins <elizabeth.martins5@gmail.com>
2023-03-06 14:19:34 -0500

    nav.jsx

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+
+const Nav = ({ user, handleLogOut }) => {
+  let userOptions
+  if (user) {
+    userOptions = (
+      <nav>
+        <h3>Welcome {user.email}!</h3>
+        <Link to="/">Home</Link>
+        <Link onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
+      </nav>
+    )
+  }
+
+  const publicOptions = (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/signIn">Sign In</Link>
+    </nav>
+  )
+
+  return (
+    <header>
+      <Link to="/">
+        <div className="logo-wrapper" alt="logo">
+          <img
+            className="logo"
+            src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="welcome banner"
+          />
+        </div>
+      </Link>
+      {user ? userOptions : publicOptions}
+    </header>
+  )
+}
+
+export default Nav
