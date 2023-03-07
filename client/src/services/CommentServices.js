11651c374676a3682e8b1ab92616499840829630
Willard Szeto <wszeto11@gmail.com>
2023-03-07 09:40:32 -0500

    ClassServices functions added

--- client/src/services/CommentServices.js
+++ client/src/services/CommentServices.js
@@ -8,3 +8,39 @@ export const GetComments = async () => {
     throw error
   }
 }
+
+export const GetCommentDetails = async () => {
+  try {
+    const res = await Client.get('api/comments/comment:id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CreateComment = async (req, res) => {
+  try {
+    const res = await Client.post('api/comments/')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateComment = async () => {
+  try {
+    const res = await Client.put('api/comments/comment:id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteComment = async () => {
+  try {
+    const res = await Client.delete('/api/comments/comment:id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
