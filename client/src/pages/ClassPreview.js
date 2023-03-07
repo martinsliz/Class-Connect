bce2a6a1c0a577aece786fa677e9824d5d490d44
Willard Szeto <wszeto11@gmail.com>
2023-03-07 17:51:36 -0500

    ClassPreview functions added

--- client/src/pages/ClassPreview.js
+++ client/src/pages/ClassPreview.js
@@ -1,31 +1,34 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Client from '../services/api'
-import { getClassBySubject } from '../services/ClassServices'
 
 const ClassPreview = ({ user }) => {
   let navigate = useNavigate()
+  const { name } = useParams()
+  //console.log(name)
   const [classes, setClasses] = useState([])
 
   useEffect(() => {
-    if (user) {
-      const handleClasses = async () => {
-        const data = await Client.get(`/api/classes/:class_subject`)
-        setClasses(data.data)
-      }
-      handleClasses()
+    //  if (user) {
+    const handleClasses = async () => {
+      const data = await Client.get(`/api/classes/subject/${name}`)
+      //  console.log(data)
+      setClasses(data.data)
     }
-  }, [user, setClasses])
+    handleClasses()
+    //  }
+  }, [])
 
   return user ? (
     <div>
       <h1 className="class-preview">Classes</h1>
-      <div className="class-container">
-        <h2>Name: {classes.name}</h2>
-        <p>Subject: {classes.subject}</p>
-        <p>Semester: {classes.semester}</p>
-        <p>Credits: {classes.credits}</p>
-      </div>
+      {classes?.map((lecture) => (
+        <div className="class-container" key={lecture.id}>
+          <h2>Name: {lecture.name}</h2>
+          <p>Semester: {lecture.semester}</p>
+          <p>Credits: {lecture.credits}</p>
+        </div>
+      ))}
     </div>
   ) : (
     <div className="protected">
