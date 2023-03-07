0cd514f707ff2368da4a90cf7db8330ff30d92e1
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-03-07 09:53:10 -0500

    add more services to ClassService.js

--- client/src/services/ClassServices.js
+++ client/src/services/ClassServices.js
@@ -8,3 +8,48 @@ export const GetClasses = async () => {
     throw error
   }
 }
+
+export const GetClassById = async () => {
+  try {
+    const res = await Client.get('/api/classes/:class_id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getClassBySubject = async () => {
+  try {
+    const res = await Client.get('/api/classes/:class_subject')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CreateClass = async () => {
+  try {
+    const res = await Client.post('/api/classes')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateClass = async () => {
+  try {
+    const res = await Client.put('/api/classes/:class_id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteClass = async () => {
+  try {
+    const res = await Client.delete('/api/classes/:class_id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
