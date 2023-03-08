f3db19121cb517e636f3165f21f40481a72c6cf3
Liz Martins <elizabeth.martins5@gmail.com>
2023-03-08 16:21:16 -0500

    comments on class details page

--- client/src/pages/ClassDetails.js
+++ client/src/pages/ClassDetails.js
@@ -1,55 +1,23 @@
 import Client from '../services/api'
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import { UpdateComment } from '../services/CommentServices'
 
-const ClassDetails = ({ user, account }) => {
+const ClassDetails = ({ user }) => {
   let navigate = useNavigate()
   const { id } = useParams()
   const [classDetails, setClassDetails] = useState({})
-  let [enrolled, setEnrolled] = useState(false)
-  let [unenrolled, setUnEnrolled] = useState(true)
+  const [comments, setComments] = useState([])
 
   useEffect(() => {
     const getClassDetails = async () => {
       const response = await Client.get(`/api/classes/${id}`)
+      console.log(response.data.comments)
       setClassDetails(response.data)
+      setComments(response.data.comments.slice(0, 10))
     }
     getClassDetails()
   }, [id])
-
-  const button = () => {
-    for (let i = 0; i < account.classes?.length; i++) {
-      if (account.classes[i].id === classDetails?.id) {
-        enrolled = true
-        unenrolled = false
-        return
-      }
-    }
-  }
-
-  button()
-
-  const handleEnrolled = async () => {
-    if (user) {
-      await Client.post(`/api/classlists/${user.id}/${classDetails.id}`)
-      await Client.put(`/api/users/updateUserDetails/${user.id}`, {
-        totalCredits: classDetails.credits + account.totalCredits
-      })
-      setEnrolled(true)
-      window.location.reload(false)
-    }
-  }
-
-  const handleUnEnrolled = async () => {
-    if (user) {
-      await Client.delete(`/api/classlists/${user.id}/${classDetails.id}`)
-      await Client.put(`/api/users/updateUserDetails/${user.id}`, {
-        totalCredits: account.totalCredits - classDetails.credits
-      })
-      setUnEnrolled(true)
-      window.location.reload(false)
-    }
-  }
 
   return user ? (
     <div className="class-content">
@@ -76,16 +44,22 @@ const ClassDetails = ({ user, account }) => {
           <div className="detail">
             <h3>Credits: {classDetails.credits}</h3>
           </div>
-          {!enrolled && (
-            <div>
-              <button onClick={handleEnrolled}>Enroll</button>
+          <button>Enroll</button>
+        </div>
+        <div>
+          <h3 className="class-preview">Comments</h3>
+          {comments?.map((comment) => (
+            <div className="comment-container" key={comment.id}>
+              <p>Comment: {comment.content}</p>
+              {/* <button onClick={() => navigate('/form/:user_id/:class_id')}>
+                Update Comment
+              </button> */}
+              <button onClick={() => UpdateComment()}>Update Comment</button>
+              <button onClick={() => navigate('/form/:user_id/:class_id')}>
+                Delete Comment
+              </button>
             </div>
-          )}
-          {!unenrolled && (
-            <div>
-              <button onClick={handleUnEnrolled}>Withdrawl</button>
-            </div>
-          )}
+          ))}
         </div>
       </section>
     </div>
